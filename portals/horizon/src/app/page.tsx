470ca737d0d8b68720c1188a9fd747e4b5068c04
TheLDB <ldb@erikboles.com>
2023-05-29 00:37:30 -0600

    updates

--- portals/horizon/src/app/page.tsx
+++ portals/horizon/src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 // TODO List
 // TODO: Display different content based on auth status
 // TODO: Implement UI Login Flow (email -> otp -> optional 2fa)
@@ -8,12 +10,17 @@ export default function Home() {
   return (
     <main
       className={
-        'w-screen min-h-screen bg-dark-primary flex justify-center items-center'
+        'w-screen min-h-screen bg-dark-primary flex flex-col justify-center items-center'
       }
     >
       <h1 className="text-6xl font-sans text-white font-bold">
         Portals via EBS via GH Workflows
       </h1>
+      <Link href={'https://github.com/withportals/portals'}>
+        <span className="text-3xl font-sans text-gray-200 font-normal">
+          GitHub
+        </span>
+      </Link>
     </main>
   );
 }
