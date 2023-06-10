ff54f7d98e90909265efa6c9f8ec527a08dc7d56
TheLDB <ldb@erikboles.com>
2023-06-10 02:23:49 -0600

    yup

--- portals/horizon/src/app/page.tsx
+++ portals/horizon/src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { PortalsNav, PortalsButton } from 'ui';
-
 // TODO List
 // TODO: Display different content based on auth status
 // TODO: Implement UI Login Flow (email -> otp -> optional 2fa)
@@ -9,29 +7,17 @@ import { PortalsNav, PortalsButton } from 'ui';
 export default function Home() {
   return (
     <>
-      <PortalsNav.NavigationMenu>
-        <PortalsNav.NavigationMenuList>
-          <PortalsNav.NavigationMenuItem>
-            <PortalsNav.NavigationMenuTrigger>
-              Item One
-            </PortalsNav.NavigationMenuTrigger>
-            <PortalsNav.NavigationMenuContent>
-              <PortalsNav.NavigationMenuLink>
-                Link
-              </PortalsNav.NavigationMenuLink>
-            </PortalsNav.NavigationMenuContent>
-          </PortalsNav.NavigationMenuItem>
-        </PortalsNav.NavigationMenuList>
-      </PortalsNav.NavigationMenu>
       <main
         className={
-          'w-screen min-h-screen bg-dark-primary flex flex-col justify-center items-center'
+          'w-screen min-h-screen bg-black flex flex-col justify-center items-center'
         }
       >
-        <h1 className="text-6xl font-sans text-white font-bold">
-          Portals via EBS via GH Workflows
+        <h1 className="text-6xl font-sans text-white font-bold pb-4">
+          Portals
         </h1>
-        <PortalsButton.Button variant={'default'}>GitHub</PortalsButton.Button>
+        <h1 className="text-4xl font-sans text-dark-accent font-bold pb-4 text-center">
+          The Farcaster Developer Platform
+        </h1>
       </main>
     </>
   );
