a5feba0824a61d540ea923ca6d9c87f2c01f3ebd
TheLDB <ldb@erikboles.com>
2023-06-16 01:34:58 -0600

    orion, change scope to hub -> sqs queue

--- farcaster/orion/src/index.ts
+++ farcaster/orion/src/index.ts
@@ -1,9 +1,38 @@
-import { env } from './utils/envsafe';
+import {env} from './utils/envsafe';
+import {getSSLHubRpcClient, HubEvent, HubEventType, HubRpcClient} from '@farcaster/hub-nodejs';
+import AWS from 'aws-sdk';
+import {sendToSQS} from './helpers/sendToSQS';
 
 export const testFunction = () => {
-  return 200;
+    return 200;
 };
 
-console.log(env);
+AWS.config.update({
+    apiVersion: env.SQS_API_VERSION
+});
+
+(async () => {
+    const client: HubRpcClient = getSSLHubRpcClient(env.HUB_RPC_URL);
+
+    const subResult = await client.subscribe({
+        eventTypes: [HubEventType.MERGE_ID_REGISTRY_EVENT, HubEventType.MERGE_NAME_REGISTRY_EVENT, HubEventType.MERGE_MESSAGE, HubEventType.PRUNE_MESSAGE, HubEventType.REVOKE_MESSAGE,]
+    });
+
+    if (subResult.isOk()) {
+        const stream = subResult.value;
+
+        for await (const anyEvent of stream) {
+            const event = anyEvent as HubEvent;
+
+            sendToSQS(event).then(response => {
+                console.log(`Message sent to SQS Queue: ${response.MessageId}`)
+            }).catch(e => {
+                console.error(`Failed to send message to the SQS Queue: ${e}`)
+            });
+        }
+    } else {
+        throw new Error('Client failed to subscribe to stream')
+    }
+})();
 
 export {};
--- farcaster/orion/src/utils/envsafe.ts
+++ farcaster/orion/src/utils/envsafe.ts
@@ -1,13 +1,21 @@
-import { envsafe, num, str } from 'envsafe';
+import {envsafe, str} from 'envsafe';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+console.warn('--------------- Node Environment ---------------');
+console.warn(process.env.NODE_ENV)
+console.warn('--------------- Node Environment ---------------');
+
+dotenv.config({
+    path: `.env.${process.env.NODE_ENV}`
+});
 
 export const env = envsafe({
-  NODE_ENV: str({
-    devDefault: 'development',
-    choices: ['development', 'test', 'production'],
-  }),
-  RANDOM_KEY: str(),
-  RANDOM_NUM: num(),
+    HUB_RPC_URL: str(),
+    SQS_URL: str(),
+    SQS_REGION: str(),
+    SQS_API_VERSION: str({
+        default: '2012-11-05'
+    }),
+    AWS_ACCESS_KEY_ID: str(),
+    AWS_SECRET_ACCESS_KEY: str(),
 });
