62097454b1e32003286273999e0967181c380314
TheLDB <ldb@erikboles.com>
2023-06-19 23:40:04 -0600

    add iam creds?

--- farcaster/orion/src/utils/envsafe.ts
+++ farcaster/orion/src/utils/envsafe.ts
@@ -16,10 +16,10 @@ export const env = envsafe({
   SQS_API_VERSION: str({
     default: '2012-11-05',
   }),
-  ACCESS_KEY_ID: str({
+  AWS_ACCESS_KEY_ID: str({
     allowEmpty: true,
   }),
-  SECRET_ACCESS_KEY: str({
+  AWS_SECRET_ACCESS_KEY: str({
     allowEmpty: true,
   }),
 });
