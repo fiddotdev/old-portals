775b6ec4409b66bed0f60e2e344ff3d223897b17
TheLDB <ldb@erikboles.com>
2023-06-29 21:36:20 -0600

    remove old creds

--- farcaster/hub-queue-consumer/src/db/connection.ts
+++ farcaster/hub-queue-consumer/src/db/connection.ts
@@ -1,13 +1,13 @@
-import {Kysely} from 'kysely';
-import {PlanetScaleDialect} from 'kysely-planetscale';
-import {Database} from './types';
+import { Kysely } from 'kysely';
+import { PlanetScaleDialect } from 'kysely-planetscale';
+import { Database } from './types';
 
 const db = new Kysely<Database>({
-    dialect: new PlanetScaleDialect({
-        host: '',
-        username: '',
-        password: '',
-    }),
+  dialect: new PlanetScaleDialect({
+    host: '',
+    username: '',
+    password: '',
+  }),
 });
 
-export {db};
+export { db };
