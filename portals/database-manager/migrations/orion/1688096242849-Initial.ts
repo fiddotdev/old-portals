e41371c892edb4e69b94ee9378745db874f1a35a
TheLDB <ldb@erikboles.com>
2023-06-29 21:38:40 -0600

    add eslint-disable for any stuff

---
+++ portals/database-manager/migrations/orion/1688096242849-Initial.ts
@@ -0,0 +1,11 @@
+import {Kysely} from 'kysely';
+
+// eslint-disable-next-line
+export async function up(db: Kysely<any>): Promise<void> {
+    // Write your migration code here
+}
+
+// eslint-disable-next-line
+export async function down(db: Kysely<any>): Promise<void> {
+    // Write your code to revert the migration here
+}
