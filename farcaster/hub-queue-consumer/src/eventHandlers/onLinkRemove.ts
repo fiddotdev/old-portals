3c65641a3ce1447a4279a2837f5c9c6c2875b840
TheLDB <ldb@erikboles.com>
2023-07-01 23:05:09 -0600

    lint

--- farcaster/hub-queue-consumer/src/eventHandlers/onLinkRemove.ts
+++ farcaster/hub-queue-consumer/src/eventHandlers/onLinkRemove.ts
@@ -1,19 +1,19 @@
-import {farcasterTimeToDate} from '../utils/farcasterTimeToDate';
-import {LinkRemoveMessage} from '@farcaster/hub-nodejs';
-import {db} from '../db/connection';
+import { farcasterTimeToDate } from '../utils/farcasterTimeToDate';
+import { LinkRemoveMessage } from '@farcaster/hub-nodejs';
+import { db } from '../db/connection';
 
 const onLinkRemove = async (messages: LinkRemoveMessage[]) => {
-    for (const message of messages) {
-        await db
-            .updateTable('links')
-            .where('fid', '=', message.data.fid)
-            // type assertion due to a problem with the type definitions. This field is infact required and present in all valid messages
-            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            .where('targetFid', '=', message.data.linkBody.targetFid!)
-            .where('type', '=', message.data.linkBody.type)
-            .set({deletedAt: farcasterTimeToDate(message.data.timestamp)})
-            .execute();
-    }
+  for (const message of messages) {
+    await db
+      .updateTable('links')
+      .where('fid', '=', message.data.fid)
+      // type assertion due to a problem with the type definitions. This field is infact required and present in all valid messages
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      .where('targetFid', '=', message.data.linkBody.targetFid!)
+      .where('type', '=', message.data.linkBody.type)
+      .set({ deletedAt: farcasterTimeToDate(message.data.timestamp) })
+      .execute();
+  }
 };
 
-export {onLinkRemove};
+export { onLinkRemove };
