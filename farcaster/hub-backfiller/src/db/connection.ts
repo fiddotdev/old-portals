942b5c2b749644b8c93f673f89b1e8b56e487a36
TheLDB <ldb@erikboles.com>
2023-07-05 04:28:50 -0600

    lint

--- farcaster/hub-backfiller/src/db/connection.ts
+++ farcaster/hub-backfiller/src/db/connection.ts
@@ -1,27 +1,27 @@
-import {Kysely} from 'kysely';
-import {OrionDB} from '@portals/database-manager';
-import {PostgresJSDialect} from 'kysely-postgres-js';
+import { Kysely } from 'kysely';
+import { OrionDB } from '@portals/database-manager';
+import { PostgresJSDialect } from 'kysely-postgres-js';
 import postgres from 'postgres';
-import {env} from '../utils/envsafe';
+import { env } from '../utils/envsafe';
 
 const db = new Kysely<OrionDB>({
-    dialect: new PostgresJSDialect({
-        connectionString: `postgresql://${env.POSTGRES_USER}:${env.POSTGRES_PASS}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`,
-        options: {
-            max: 10,
-            types: {
-                bigint: {
-                    to: 20,
-                    from: 20,
-                    // eslint-disable-next-line
-                    parse: (x: any) => Number(x),
-                    // eslint-disable-next-line
-                    serialize: (x: any) => x.toString(),
-                },
-            },
+  dialect: new PostgresJSDialect({
+    connectionString: `postgresql://${env.POSTGRES_USER}:${env.POSTGRES_PASS}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`,
+    options: {
+      max: 10,
+      types: {
+        bigint: {
+          to: 20,
+          from: 20,
+          // eslint-disable-next-line
+          parse: (x: any) => Number(x),
+          // eslint-disable-next-line
+          serialize: (x: any) => x.toString(),
         },
-        postgres,
-    }),
+      },
+    },
+    postgres,
+  }),
 });
 
-export {db};
+export { db };
