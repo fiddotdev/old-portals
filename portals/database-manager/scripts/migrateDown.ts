59e62e80adf27dc6357b5e3e2b482c07387f3c60
TheLDB <ldb@erikboles.com>
2023-07-05 05:21:22 -0600

    update db manager

--- portals/database-manager/scripts/migrateDown.ts
+++ portals/database-manager/scripts/migrateDown.ts
@@ -1,96 +1,99 @@
 import * as path from 'path';
-import {promises as fs} from 'fs';
-import {FileMigrationProvider, Kysely, Migrator,} from 'kysely';
-import {OrionDB} from '../src';
+import { promises as fs } from 'fs';
+import { FileMigrationProvider, Kysely, Migrator } from 'kysely';
+import { OrionDB } from '../src';
 import * as readline from 'readline';
-import {env} from '../src/utils/envSafe';
-import {PostgresJSDialect, PostgresJSDialectConfig} from 'kysely-postgres-js';
+import { env } from '../src/utils/envSafe';
+import { PostgresJSDialect, PostgresJSDialectConfig } from 'kysely-postgres-js';
 import postgres from 'postgres';
 
 async function migrateDown() {
-    const args = process.argv.slice(2);
-    const nameArg = args.find((arg) => arg.startsWith('--name='));
+  const args = process.argv.slice(2);
+  const nameArg = args.find((arg) => arg.startsWith('--name='));
 
-    if (nameArg) {
-        const formattedNameArg = nameArg.split('=')[1];
+  if (nameArg) {
+    const formattedNameArg = nameArg.split('=')[1];
 
-        let poolConfig: PostgresJSDialectConfig;
+    let poolConfig: PostgresJSDialectConfig;
 
-        switch (formattedNameArg) {
-            case 'orion': {
-                poolConfig = {
-                    connectionString: `postgresql://${env.ORION_POSTGRES_USERNAME}:${env.ORION_POSTGRES_PASSWORD}@${env.ORION_POSTGRES_HOST}:${env.ORION_POSTGRES_PORT}/${env.ORION_POSTGRES_DB}`,
-                    options: {
-                        max: 10
-                    },
-                    postgres
-                };
-                break;
-            }
-            default: {
-                throw new Error('Please select a valid package.');
-            }
+    switch (formattedNameArg) {
+      case 'orion': {
+        poolConfig = {
+          connectionString: `postgresql://${env.ORION_POSTGRES_USERNAME}:${env.ORION_POSTGRES_PASSWORD}@${env.ORION_POSTGRES_HOST}:${env.ORION_POSTGRES_PORT}/${env.ORION_POSTGRES_DB}`,
+          options: {
+            max: 10,
+          },
+          postgres,
+        };
+        break;
+      }
+      default: {
+        throw new Error('Please select a valid package.');
+      }
+    }
+
+    const db = new Kysely<OrionDB>({
+      dialect: new PostgresJSDialect(poolConfig),
+    });
+
+    const migrator = new Migrator({
+      db,
+      provider: new FileMigrationProvider({
+        fs,
+        path,
+        migrationFolder: path.join(
+          __dirname,
+          '..',
+          'migrations',
+          formattedNameArg
+        ),
+      }),
+    });
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(
+      `Are you sure you want to migrate the package ${formattedNameArg} down? (yes/no)\n`,
+      async (question) => {
+        if (question === 'yes') {
+          console.log('Starting Migration Down');
+          const { error, results } = await migrator.migrateDown();
+
+          if (results && results[0]) {
+            console.log(
+              `migration "${results[0].migrationName}" was reverted successfully`
+            );
+          }
+
+          if (error) {
+            console.error('failed to revert migration');
+            console.error(error);
+            process.exit(1);
+          }
         }
 
-        const db = new Kysely<OrionDB>({
-            dialect: new PostgresJSDialect(poolConfig),
-        });
+        rl.close();
+      }
+    );
 
-        const migrator = new Migrator({
-            db,
-            provider: new FileMigrationProvider({
-                fs,
-                path,
-                migrationFolder: path.join(
-                    __dirname,
-                    '..',
-                    'migrations',
-                    formattedNameArg
-                ),
-            }),
-        });
-
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
-
-        rl.question(
-            `Are you sure you want to migrate the package ${formattedNameArg} down? (yes/no)\n`,
-            async (question) => {
-                if (question === 'yes') {
-                    console.log('Starting Migration Down');
-                    const {error, results} = await migrator.migrateDown();
-
-                    if (results && results[0]) {
-                        console.log(
-                            `migration "${results[0].migrationName}" was reverted successfully`
-                        );
-                    }
-
-                    if (error) {
-                        console.error('failed to revert migration');
-                        console.error(error);
-                        process.exit(1);
-                    }
-                }
-
-                rl.close();
-            }
-        );
-
-        await db.destroy().then(() => {
-            console.log('Destroyed DB successfuly');
-        }).catch(() => {
-            console.error('Failed to destroy DB!')
-        });
-    } else {
-        console.error(
-            'Please provide a valid package to migrate with the --name= parameter'
-        );
-    }
+    await db
+      .destroy()
+      .then(() => {
+        console.log('Destroyed DB successfuly');
+      })
+      .catch(() => {
+        console.error('Failed to destroy DB!');
+      });
+  } else {
+    console.error(
+      'Please provide a valid package to migrate with the --name= parameter'
+    );
+  }
 }
 
 (async () => {
-    await migrateDown();
+  await migrateDown();
 })();
--- portals/database-manager/scripts/migrateUp.ts
+++ portals/database-manager/scripts/migrateUp.ts
@@ -1,68 +1,76 @@
 import * as path from 'path';
-import {promises as fs} from 'fs';
-import {FileMigrationProvider, Kysely, Migrator, PostgresDialect,} from 'kysely';
-import {Pool, PoolConfig} from 'pg';
+import { promises as fs } from 'fs';
+import {
+  FileMigrationProvider,
+  Kysely,
+  Migrator,
+  PostgresDialect,
+} from 'kysely';
+import { Pool, PoolConfig } from 'pg';
 import * as readline from 'readline';
-import {env} from '../src/utils/envSafe';
-import {OrionDB} from '../src';
+import { env } from '../src/utils/envSafe';
+import { OrionDB } from '../src';
 
 export async function migrateToLatest(packageName: string) {
-    let poolConfig: PoolConfig;
+  let poolConfig: PoolConfig;
 
-    const allowedPackages = ['orion'];
-    switch (packageName) {
-        case 'orion': {
-            poolConfig = {
-                host: env.ORION_POSTGRES_HOST,
-                user: env.ORION_POSTGRES_USERNAME,
-                password: env.ORION_POSTGRES_PASSWORD,
-                port: env.ORION_POSTGRES_PORT,
-                database: env.ORION_POSTGRES_DB,
-            };
-            break;
-        }
-        default: {
-            throw new Error(
-                `Please select a valid package. One of: ${allowedPackages.join(', ')}`
-            );
-        }
+  const allowedPackages = ['orion'];
+  switch (packageName) {
+    case 'orion': {
+      poolConfig = {
+        host: env.ORION_POSTGRES_HOST,
+        user: env.ORION_POSTGRES_USERNAME,
+        password: env.ORION_POSTGRES_PASSWORD,
+        port: env.ORION_POSTGRES_PORT,
+        database: env.ORION_POSTGRES_DB,
+      };
+      break;
     }
-
-    const db = new Kysely<OrionDB>({
-        dialect: new PostgresDialect({
-            pool: new Pool(poolConfig),
-        }),
-    });
-
-    const migrator = new Migrator({
-        db,
-        provider: new FileMigrationProvider({
-            fs,
-            path,
-            migrationFolder: path.join(__dirname, '..', 'migrations', packageName),
-        }),
-    });
-
-    const {error, results} = await migrator.migrateToLatest();
-
-    results?.forEach((it) => {
-        if (it.status === 'Success') {
-            console.log(`migration "${it.migrationName}" was executed successfully`);
-        } else if (it.status === 'Error') {
-            console.error(`failed to execute migration "${it.migrationName}"`);
-        }
-    });
-
-    if (error) {
-        console.error('failed to migrate');
-        console.error(error);
-        process.exit(1);
+    default: {
+      throw new Error(
+        `Please select a valid package. One of: ${allowedPackages.join(', ')}`
+      );
     }
+  }
 
-    await db.destroy().then(() => {
-        console.log('Destroyed DB successfuly');
-    }).catch(() => {
-        console.error('Failed to destroy DB!')
+  const db = new Kysely<OrionDB>({
+    dialect: new PostgresDialect({
+      pool: new Pool(poolConfig),
+    }),
+  });
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(__dirname, '..', 'migrations', packageName),
+    }),
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach((it) => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error('failed to migrate');
+    console.error(error);
+    process.exit(1);
+  }
+
+  await db
+    .destroy()
+    .then(() => {
+      console.log('Destroyed DB successfuly');
+    })
+    .catch(() => {
+      console.error('Failed to destroy DB!');
     });
 }
 
@@ -71,32 +79,32 @@ const nameArg = args.find((arg) => arg.startsWith('--name='));
 
 const allowedPackages = ['orion'];
 if (nameArg) {
-    if (allowedPackages.includes(nameArg.split('=')[1])) {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-        });
+  if (allowedPackages.includes(nameArg.split('=')[1])) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-        rl.question(
-            `Are you sure you want to migrate up on package '${
-                nameArg.split('=')[1]
-            }'? yes/no\n`,
-            (answer) => {
-                if (answer === 'yes') {
-                    migrateToLatest(nameArg.split('=')[1]).then(() => {
-                        console.log('Migrated to latest');
-                    });
-                }
-                rl.close();
-            }
-        );
-    } else {
-        console.error(
-            `Please provide a valid package, one of: ${allowedPackages.join(', ')}`
-        );
-    }
-} else {
-    console.error(
-        'Please provide a valid package to migrate with the --name= parameter'
+    rl.question(
+      `Are you sure you want to migrate up on package '${
+        nameArg.split('=')[1]
+      }'? yes/no\n`,
+      (answer) => {
+        if (answer === 'yes') {
+          migrateToLatest(nameArg.split('=')[1]).then(() => {
+            console.log('Migrated to latest');
+          });
+        }
+        rl.close();
+      }
     );
+  } else {
+    console.error(
+      `Please provide a valid package, one of: ${allowedPackages.join(', ')}`
+    );
+  }
+} else {
+  console.error(
+    'Please provide a valid package to migrate with the --name= parameter'
+  );
 }
