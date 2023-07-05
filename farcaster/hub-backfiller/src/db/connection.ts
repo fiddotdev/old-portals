746de30abe24e8f7351796e40e7dc103071cc003
TheLDB <ldb@erikboles.com>
2023-07-05 04:28:43 -0600

    remove comment

--- farcaster/hub-backfiller/src/db/connection.ts
+++ farcaster/hub-backfiller/src/db/connection.ts
@@ -1,46 +1,27 @@
-import { Kysely } from 'kysely';
-import { OrionDB } from '@portals/database-manager';
-import { PostgresJSDialect } from 'kysely-postgres-js';
+import {Kysely} from 'kysely';
+import {OrionDB} from '@portals/database-manager';
+import {PostgresJSDialect} from 'kysely-postgres-js';
 import postgres from 'postgres';
-import { env } from '../utils/envsafe';
-// return new Kysely<Database>({
-//     dialect: new PostgresJSDialect({
-//         connectionString,
-//         options: {
-//             max: 10,
-//             types: {
-//                 // BigInts will not exceed Number.MAX_SAFE_INTEGER for our use case.
-//                 // Return as JavaScript's `number` type so it's easier to work with.
-//                 bigint: {
-//                     to: 20,
-//                     from: 20,
-//                     parse: (x: any) => Number(x),
-//                     serialize: (x: any) => x.toString(),
-//                 },
-//             },
-//         },
-//         postgres,
-//     }),
-//     plugins: [new CamelCasePlugin()],
-// });
+import {env} from '../utils/envsafe';
+
 const db = new Kysely<OrionDB>({
-  dialect: new PostgresJSDialect({
-    connectionString: `postgresql://${env.POSTGRES_USER}:${env.POSTGRES_PASS}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`,
-    options: {
-      max: 10,
-      types: {
-        bigint: {
-          to: 20,
-          from: 20,
-          // eslint-disable-next-line
-          parse: (x: any) => Number(x),
-          // eslint-disable-next-line
-          serialize: (x: any) => x.toString(),
+    dialect: new PostgresJSDialect({
+        connectionString: `postgresql://${env.POSTGRES_USER}:${env.POSTGRES_PASS}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`,
+        options: {
+            max: 10,
+            types: {
+                bigint: {
+                    to: 20,
+                    from: 20,
+                    // eslint-disable-next-line
+                    parse: (x: any) => Number(x),
+                    // eslint-disable-next-line
+                    serialize: (x: any) => x.toString(),
+                },
+            },
         },
-      },
-    },
-    postgres,
-  }),
+        postgres,
+    }),
 });
 
-export { db };
+export {db};
