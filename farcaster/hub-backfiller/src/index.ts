e562449aa7e58c1c5dcb83846333eb21153b1b0a
TheLDB <ldb@erikboles.com>
2023-07-05 05:40:55 -0600

    omg

--- farcaster/hub-backfiller/src/index.ts
+++ farcaster/hub-backfiller/src/index.ts
@@ -1,55 +1,64 @@
-import { hubClient } from './utils/hubClient';
-import { BackfillError } from './types/BackfillError';
-import fastq, { queueAsPromised } from 'fastq';
+import {hubClient} from './utils/hubClient';
+import {BackfillError} from './types/BackfillError';
+import fastq, {queueAsPromised} from 'fastq';
 import prettyMilliseconds from 'pretty-ms';
-import { processAllMessagesForFid } from './helpers/processAllMessagesForFid';
-import { env } from './utils/envsafe';
+import {processAllMessagesForFid} from './helpers/processAllMessagesForFid';
+import {env} from './utils/envsafe';
+import {db} from './db/connection';
 
 const MAX_JOB_CONCURRENCY = env.MAX_CONCURRENCY;
 
 const backfill = async () => {
-  const maxFidResult = await hubClient.getFids({ pageSize: 1, reverse: true });
-  if (maxFidResult.isErr())
-    throw new BackfillError('Unable to backfill', {
-      cause: maxFidResult.error,
-    });
+    const maxFidResult = await hubClient.getFids({pageSize: 1, reverse: true});
+    if (maxFidResult.isErr())
+        throw new BackfillError('Unable to backfill', {
+            cause: maxFidResult.error,
+        });
 
-  const maxFid = maxFidResult.value.fids[0];
-  let totalProcessed = 0;
+    const maxFid = maxFidResult.value.fids[0];
+    let totalProcessed = 0;
 
-  const startTime = Date.now();
-  const queue: queueAsPromised<{ fid: number }> = fastq.promise(
-    async ({ fid }) => {
-      await processAllMessagesForFid(fid);
+    const startTime = Date.now();
+    const queue: queueAsPromised<{ fid: number }> = fastq.promise(
+        async ({fid}) => {
+            await processAllMessagesForFid(fid);
 
-      totalProcessed += 1;
-      const elapsedMs = Date.now() - startTime;
-      const millisRemaining = Math.ceil(
-        (elapsedMs / totalProcessed) * (maxFid - totalProcessed)
-      );
+            totalProcessed += 1;
+            const elapsedMs = Date.now() - startTime;
+            const millisRemaining = Math.ceil(
+                (elapsedMs / totalProcessed) * (maxFid - totalProcessed)
+            );
 
-      console.info(
-        `[Backfill] Completed FID ${fid}/${maxFid}. Estimated time remaining: ${prettyMilliseconds(
-          millisRemaining
-        )}`
-      );
-    },
-    MAX_JOB_CONCURRENCY
-  );
+            console.info(
+                `[Backfill] Completed FID ${fid}/${maxFid}. Estimated time remaining: ${prettyMilliseconds(
+                    millisRemaining
+                )}`
+            );
+        },
+        MAX_JOB_CONCURRENCY
+    );
 
-  for (let fid = 1; fid <= maxFid; fid++) {
-    queue.push({ fid });
-  }
+    for (let fid = 1; fid <= maxFid; fid++) {
+        queue.push({fid});
+    }
 
-  await queue.drained();
+    await queue.drained();
 
-  console.info(
-    `[Backfill] Completed in ${prettyMilliseconds(Date.now() - startTime)}`
-  );
+    console.info(
+        `[Backfill] Completed in ${prettyMilliseconds(Date.now() - startTime)}`
+    );
 };
 
 (async () => {
-  await backfill();
+    await backfill().then(async () => {
+        await db.destroy().then(() => {
+            console.log('Destroyed DB!')
+            process.exit(0);
+        }).catch(() => {
+            console.log('Failed to Destroy DB!!');
+            process.exit(1);
+        });
+    });
 })();
 
 export {};
--- farcaster/hub-queue-consumer/src/db/connection.ts
+++ farcaster/hub-queue-consumer/src/db/connection.ts
@@ -1,17 +1,26 @@
-import { Kysely, PostgresDialect } from 'kysely';
+import { Kysely } from 'kysely';
+import { PostgresJSDialect } from 'kysely-postgres-js';
 import { OrionDB } from '@portals/database-manager';
-import { Pool } from 'pg';
+import postgres from 'postgres';
 import { env } from '../utils/envsafe';
 
 const db = new Kysely<OrionDB>({
-  dialect: new PostgresDialect({
-    pool: new Pool({
-      host: env.POSTGRES_HOST,
-      user: env.POSTGRES_USER,
-      password: env.POSTGRES_PASS,
-      port: env.POSTGRES_PORT,
-      database: env.POSTGRES_DB,
-    }),
+  dialect: new PostgresJSDialect({
+    connectionString: `postgresql://${env.POSTGRES_USER}:${env.POSTGRES_PASS}@${env.POSTGRES_HOST}:${env.POSTGRES_PORT}/${env.POSTGRES_DB}`,
+    options: {
+      max: 10,
+      types: {
+        bigint: {
+          to: 20,
+          from: 20,
+          // eslint-disable-next-line
+          parse: (x: any) => Number(x),
+          // eslint-disable-next-line
+          serialize: (x: any) => x.toString(),
+        },
+      },
+    },
+    postgres,
   }),
 });
 
