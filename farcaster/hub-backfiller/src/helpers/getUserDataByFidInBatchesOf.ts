8b4376365068542e893152b25ad6de9917096dba
TheLDB <ldb@erikboles.com>
2023-07-06 13:10:10 -0600

    bump queue-consumer & orion to redeploy

--- farcaster/hub-backfiller/src/helpers/getUserDataByFidInBatchesOf.ts
+++ farcaster/hub-backfiller/src/helpers/getUserDataByFidInBatchesOf.ts
@@ -1,26 +1,26 @@
-import { hubClient } from '../utils/hubClient';
-import { BackfillError } from '../types/BackfillError';
-import { Message } from '@farcaster/hub-nodejs';
+import {hubClient} from '../utils/hubClient';
+import {BackfillError} from '../types/BackfillError';
+import {Message} from '@farcaster/hub-nodejs';
 
 async function* getUserDataByFidInBatchesOf(
-  fid: number,
-  pageSize: number
+    fid: number,
+    pageSize: number
 ): AsyncGenerator<Message[], void, unknown> {
-  let result = await hubClient.getUserDataByFid({ pageSize, fid });
+    let result = await hubClient.getUserDataByFid({pageSize, fid});
 
-  for (;;) {
-    if (result.isErr()) {
-      throw new BackfillError('Unable to backfill', { cause: result.error });
+    for (; ;) {
+        if (result.isErr()) {
+            throw new BackfillError('Unable to backfill', {cause: result.error});
+        }
+ 
+        const {messages, nextPageToken: pageToken} = result.value;
+
+        yield messages;
+
+        if (!pageToken?.length) break;
+
+        result = await hubClient.getUserDataByFid({pageSize, pageToken, fid});
     }
-
-    const { messages, nextPageToken: pageToken } = result.value;
-
-    yield messages;
-
-    if (!pageToken?.length) break;
-
-    result = await hubClient.getUserDataByFid({ pageSize, pageToken, fid });
-  }
 }
 
-export { getUserDataByFidInBatchesOf };
+export {getUserDataByFidInBatchesOf};
